a815294e64daaa26622e8940f7bf906ffa0cc6e9
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2025-05-01 13:34:02 +0300

    Integrating Backend & Frontend

---
+++ Backend/src/main/java/com/DocumentCollaborator/DocumentCollaborator/DocumentCollaboratorApplication.java
@@ -0,0 +1,11 @@
+package com.DocumentCollaborator.DocumentCollaborator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentCollaboratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentCollaboratorApplication.class, args);
+	}
+}
---
+++ Backend/src/test/java/com/DocumentCollaborator/DocumentCollaborator/DocumentCollaboratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DocumentCollaborator.DocumentCollaborator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentCollaboratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
